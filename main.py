2e915fdca8fac4f392d6365ec4ee01dca4087835
cw-roy <curtis_roy@msn.com>
2023-06-12 14:08:43 -0700

    Update main, rebuild Docker image

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-#########################################################################
-### THIS IS THE DOCKERIZED VERSION FOR POTENTIAL USE IN FUTURE STATE. ###
-#########################################################################
-
 """Converts video files to .mp4 using FFmpeg."""
 
 import os
@@ -26,7 +22,7 @@ def scan_directory():
                 matching_files.append(file_name.path)
             else:
                 log_messages.append(
-                    f'"{file_name.name}" cannot be converted to .mp4 using FFmpeg.'
+                    f'"{file_name.name}" cannot be converted to .mp4.'
                 )
         else:
             log_messages.append(f'"{file_name.name}" is not a file.')
@@ -70,18 +66,34 @@ def check_file_convertibility(file_path):
         return False
 
 
+def get_file_size(file_size):
+    """
+    Converts the given file size in bytes to a human-readable format (e.g., KB, MB, GB).
+    Returns a string representing the human-readable file size.
+    """
+    for unit in ["B", "KB", "MB", "GB"]:
+        if file_size < 1024.0:
+            return f"{file_size:.1f} {unit}"
+        file_size /= 1024.0
+
+
 def convert_files(file_paths, log_file):
     """
     Converts each file in the provided list to mp4 using FFmpeg.
-        Args:
+    Args:
         file_paths (list): A list of file paths to convert.
         log_file (file): The log file to write conversion logs to.
     """
-    log_messages = []
+    log_file.write("Conversion Log\n")
+    log_file.write("-----------------\n\n")
+
     converted_folder = Path(__file__).parent / "converted_media"
     converted_folder.mkdir(exist_ok=True)
 
     start_time = time.time()  # Add start time for the entire conversion process
+
+    original_total_size = 0
+    final_total_size = 0
 
     for file_path in file_paths:
         try:
@@ -89,6 +101,9 @@ def convert_files(file_paths, log_file):
             file_prefix = Path(file_path).stem
 
             file_prefix = file_prefix.replace(" ", "_")
+
+            original_file_size = os.path.getsize(file_path)
+            original_total_size += original_file_size
 
             counter = 1
             output_file_path = converted_folder / f"{file_prefix}_converted.mp4"
@@ -105,23 +120,31 @@ def convert_files(file_paths, log_file):
                 check=True,
             )
 
-            log_messages.append(
-                f'"{file_name}" was converted to "{output_file_path.name}"'
+            new_file_size = os.path.getsize(output_file_path)
+            final_total_size += new_file_size
+
+            elapsed_time = time.time() - start_time  # Calculate the elapsed time
+            minutes, seconds = divmod(elapsed_time, 60)
+
+            log_message = (
+                f'"{file_name}" ({get_file_size(original_file_size)}) was converted to "{output_file_path.name}" '
+                f"({get_file_size(new_file_size)}) in {minutes:.0f}m{seconds:.0f}s."
             )
+            log_file.write(log_message + "\n")
 
         except subprocess.CalledProcessError as errors:
-            log_messages.append(
-                f'Error converting "{file_path}": {errors.stdout.strip()}'
-            )
+            log_file.write(f'Error converting "{file_path}": {errors.stdout.strip()}\n')
 
     elapsed_time = time.time() - start_time  # Calculate the elapsed time
     minutes, seconds = divmod(elapsed_time, 60)
 
+    log_file.write("\nSummary\n")
+    log_file.write("-----------------\n")
     log_file.write(f"Start Time: {time.ctime(start_time)}\n")
     log_file.write(f"End Time: {time.ctime(time.time())}\n")
     log_file.write(f"Elapsed Time: {minutes:.0f}m{seconds:.0f}s\n")
-    log_file.write("\n".join(log_messages))
-    log_file.write("\n")
+    log_file.write(f"Original total file size: {get_file_size(original_total_size)}\n")
+    log_file.write(f"Final total file size: {get_file_size(final_total_size)}\n")
 
 
 def main():
@@ -137,6 +160,8 @@ def main():
 
     with open(log_file_name, "w", encoding="utf-8") as log_file:
         matching_files, scan_log_messages = scan_directory()
+        log_file.write("Scan Log\n")
+        log_file.write("-----------------\n")
         log_file.write("\n".join(scan_log_messages))
         log_file.write("\n\n")
 
@@ -145,7 +170,11 @@ def main():
 
         convert_files(matching_files, log_file)
 
-    print(f'Conversion complete. Log file saved to "{log_file_name}".')
+    print(f'\nConversion complete. Log file saved to "{log_file_name}".\n')
+
+    # Print log file contents to the console
+    with open(log_file_name, "r", encoding="utf-8") as log_file:
+        print(log_file.read())
 
 
 if __name__ == "__main__":
